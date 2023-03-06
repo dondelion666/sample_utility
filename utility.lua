0d781277e1a962da1529c80737f23c3418b0517c
dondelion666 <91913477+dondelion666@users.noreply.github.com>
2023-03-06 19:43:21 +0000

    Update utility.lua

--- utility.lua
+++ utility.lua
@@ -1,9 +1,5 @@
 function init()
   
-  audio.monitor_mono ()  
-  audio.level_adc_cut(1)
-  
-  --RECORDING
   softcut.enable(1,1)
   softcut.buffer(1,1)
   softcut.position(1,1)
@@ -12,39 +8,22 @@ function init()
   softcut.pre_level(1,0)
   softcut.level_input_cut(1,1,1)
   softcut.rec(1,1) 
-  --input only
-  softcut.level(1,0)
-  --no playback
-  softcut.play(1,0)
+  softcut.level(1,1)
+  softcut.play(1,1)
+
   --5mins recording then loop
   softcut.loop(1,1)
   softcut.loop_start(1,1)
   softcut.loop_end(1,300)
   
-  
-  
 end
 
-function key(n,z)
-  
-  if n==2 and z==1 then
-    softcut.position(1,1)
-    softcut.rec_level(1,1)
-    audio.level_monitor(1)
-  end
-  
-  if n==2 and z==0 then
-    softcut.rec_level(1,0)
-    audio.level_monitor(0)
-  end
-  
-  if n==3 and z==1 then
-    softcut.position(1,1)
-    softcut.level(1,1)
-  end
-  
-  if n==3 and z==0 then
-    softcut.level(1,0)
-  end
-  
-end
+params:add_file("file"_path.audio)
+params:set_action("file",function(x) buffer_read_stereo(x,0,1,-1,0,1) end)
+
+params:add_control("start","start",controlspec.new(0,300,'lin',0.01,0,'s',0.01))
+params:set_action("start",function(x) softcut.loop_start(x) end)
+params:add_control("end","end",controlspec.new(0,300,'lin',0.01,0,'s',0.01))
+params:set_action("end",function(x) softcut.loop_end(x) end)
+
+params:bang()
