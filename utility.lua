ac4825fce2821116ba6fed3a84b5ea12c587de4a
dondelion666 <91913477+dondelion666@users.noreply.github.com>
2023-03-06 19:17:56 +0000

    Create utility.lua

---
+++ utility.lua
@@ -0,0 +1,50 @@
+function init()
+  
+  audio.monitor_mono ()  
+  audio.level_adc_cut(1)
+  
+  --RECORDING
+ 
+  softcut.enable(1,1)
+  softcut.buffer(1,1)
+  
+  --input only
+  softcut.level(1,0)
+ 
+  
+  softcut.play(1,1)
+  softcut.rec(1,1)
+  softcut.loop(1,0)
+  softcut.loop_start(1,1)
+  softcut.position(1,1)
+  softcut.rate(1,1)
+  softcut.rec_level(1,0)
+  softcut.pre_level(1,1)
+  softcut.level_input_cut(1,1,1)
+  softcut.loop_end(1,5)
+  
+end
+
+function key(n,z)
+  
+  if n==2 and z==1 then
+    softcut.position(1,1)
+    softcut.rec_level(1,1)
+    audio.level_monitor(1)
+  end
+  
+  if n==2 and z==0 then
+    softcut.rec_level(1,0)
+    audio.level_monitor(0)
+  end
+  
+  if n==3 and z==1 then
+    softcut.position(1,1)
+    softcut.level(1,1)
+  end
+  
+  if n==3 and z==0 then
+    softcut.level(1,0)
+  end
+  
+end
